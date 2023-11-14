d6df1d2062ab92e0031a23919b79d165b53cb477
RaphaW <raphael-weber-bvs@gmx.de>
2023-11-14 14:37:44 +0100

    minor adjustments

--- src/main.py
+++ src/main.py
@@ -2,17 +2,21 @@ import typer
 
 app = typer.Typer()
 
+
 @app.command()
 def generate():
     print("generating")
 
+
 @app.command()
-def augmentate():
-    print("augmentating")
+def process():
+    print("processing")
+
 
 @app.command()
 def evaluate():
     print("evaluating")
+
 
 if __name__ == "__main__":
     app()
