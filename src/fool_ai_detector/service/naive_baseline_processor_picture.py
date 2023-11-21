dca8e924ac97280821b25d9ee2ce96443c42690e
EhrenbruderRoschtbraten <lukas.mueller@nc-online.de>
2023-11-21 15:18:49 +0100

    removed inline comments

--- src/fool_ai_detector/service/naive_baseline_processor_picture.py
+++ src/fool_ai_detector/service/naive_baseline_processor_picture.py
@@ -10,11 +10,7 @@ class NaiveBaselineProcessorImage(Processor):
         """
         Adds some random noise to an image to augment it.
         """
-        # read image file,
         image = skimage.io.imread(input_file_path)
-        # augment the image
         processed_image = skimage.util.random_noise(image, mode='gaussian', clip=True)
-        # convert pixel representation from [0..1] to [0..255]
         processed_image = (processed_image * 255).astype(np.uint8)
-        # save image to given location
         skimage.io.imsave(output_file_path, processed_image, check_contrast=False)
