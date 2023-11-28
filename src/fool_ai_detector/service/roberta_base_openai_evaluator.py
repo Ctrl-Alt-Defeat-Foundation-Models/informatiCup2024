0f13bf04e65d89e81a415ce3e0ebadcdae5f12ab
Max Buchholz <Max.Buchholz@gmx.de>
2023-11-28 14:12:12 +0100

    roberta base openai detector (#19)
    * Implement roberta_base_openai_detector.py and test
    
    * Fix tests and issues found with test
    
    * Update test/fool_ai_detector/service/test_fake_generator_picture.py
    
    Co-authored-by: EhrenbruderRoschtbraten <81183139+EhrenbruderRoschtbraten@users.noreply.github.com>
    
    * Update test/fool_ai_detector/service/test_fake_generator_text.py
    
    Co-authored-by: EhrenbruderRoschtbraten <81183139+EhrenbruderRoschtbraten@users.noreply.github.com>
    
    * Update src/fool_ai_detector/service/fake_generator_text.py
    
    Co-authored-by: EhrenbruderRoschtbraten <81183139+EhrenbruderRoschtbraten@users.noreply.github.com>
    
    * Update src/fool_ai_detector/service/fake_generator_picture.py
    
    Co-authored-by: EhrenbruderRoschtbraten <81183139+EhrenbruderRoschtbraten@users.noreply.github.com>
    
    * make path fitting
    ignore whole idea ordner
    
    * update cached files to ignore misc folder:
    git rm --cached -r .idea
    
    * Update test/README.md
    
    Co-authored-by: RaphaW <116720366+RaphaW@users.noreply.github.com>
    
    * Added a simple commandline interface (#18)
    
    * installed and imported typer
    
    * added main.py
    
    * added pyproject.toml
    
    * minor adjustments
    
    * finalized the main.py for this issue
    
    * deleted comma
    
    Co-authored-by: Max Buchholz <Max.Buchholz@gmx.de>
    
    * add new line
    
    Co-authored-by: Max Buchholz <Max.Buchholz@gmx.de>
    
    * added a README.md
    
    * Updated the README.md
    
    * deleted the original README.md and move mine to an outer layer
    
    ---------
    
    Co-authored-by: Max Buchholz <Max.Buchholz@gmx.de>
    
    * Add missing param description
    
    * Add missing dependency to pyproject.toml
    
    * Add missing dependency to pyproject.toml
    
    * Rename detector to evaluator
    
    * Rename variables in test
    
    * Add return tag to docstring
    
    Co-authored-by: LeviUniAcc <102458061+LeviUniAcc@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: LeviUniAcc <102458061+LeviUniAcc@users.noreply.github.com>
    Co-authored-by: EhrenbruderRoschtbraten <81183139+EhrenbruderRoschtbraten@users.noreply.github.com>
    Co-authored-by: LeviUniAcc <otterbach.levi@gmail.com>
    Co-authored-by: RaphaW <116720366+RaphaW@users.noreply.github.com>

---
+++ src/fool_ai_detector/service/roberta_base_openai_evaluator.py
@@ -0,0 +1,27 @@
+"""
+Evaluator based on the roberta_base_openai_detector
+"""
+from fool_ai_detector.model.evaluator import Evaluator
+from transformers import pipeline
+
+
+class RobertaBaseEvaluator(Evaluator):
+    """
+    Base class for an Evaluator
+    """
+
+    def evaluate(self, input_file_path) -> bool:
+        """
+        Evaluates a given text based on the specific detector
+        :param input_file_path: path to the file, that should be evaluated
+        :return: bool; true = fake, false = real
+        """
+        file = open(input_file_path, "r")
+        text = file.read()
+        pipe = pipeline("text-classification", model="roberta-base-openai-detector")
+        output = pipe(text)
+        label = output[0]["label"]
+        if label == "Real":
+            return False
+        else:
+            return True
