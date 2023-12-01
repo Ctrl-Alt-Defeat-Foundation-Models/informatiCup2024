a0cc82f1e965ea7fe19845daf5e0a950187a45e4
LukasMueller3 <lukas.mueller@nc-online.de>
2023-12-01 12:42:26 +0100

    added test class, have to test on laptop

--- src/fool_ai_detector/service/stable_diffusion_image_generator.py
+++ src/fool_ai_detector/service/stable_diffusion_image_generator.py
@@ -4,6 +4,7 @@ Generator based on the Stable Diffusion model
 import skimage
 import torch
 from diffusers import StableDiffusionPipeline
+
 from fool_ai_detector.model.generator import Generator
 
 
