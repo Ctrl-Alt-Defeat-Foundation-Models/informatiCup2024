ef9cd3d138f5d5c18d07592edd54a545193ddd5c
LukasMueller3 <lukas.mueller@nc-online.de>
2023-12-03 15:46:31 +0100

    updated white space to white-space

--- src/fool_ai_detector/service/naive_baseline_processor_text.py
+++ src/fool_ai_detector/service/naive_baseline_processor_text.py
@@ -6,7 +6,7 @@ from fool_ai_detector.model.processor import Processor
 class NaiveBaselineProcessorText(Processor):
     def process(self, input_file_path, output_file_path):
         """
-        Replaces every occurrence of a white space with double white space to augment a text.
+        Replaces every occurrence of a white-space with double white-space to augment a text.
         """
         text = Path(input_file_path).read_text()
         augmented_text = text.replace(" ", "  ")
