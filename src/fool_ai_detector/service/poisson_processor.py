6556cf8f6fcce1dd0285dcceb1c1a23bff5039f2
RaphaW <116720366+RaphaW@users.noreply.github.com>
2023-12-19 16:27:18 +0100

    Poisson processor (#53)
    * Added the poisson processor
    
    * Added the poisson processor
    
    * Added the option to run the pipeline multiple times (#51)
    
    * Added the option to run the pipeline multiple times
    
    * Deleted random stuff
    
    * Added the option to run the pipeline multiple times
    
    * Deleted 2 lines that were redundant
    
    * Translator processor (#55)
    
    * Added a translator processor
    
    * Added a translator processor
    
    * add translator to toml and made a split text workaround for the translator >1000
    
    * fix roberta bug <514 words
    
    ---------
    
    Co-authored-by: RaphaW <raphael-weber-bvs@gmx.de>
    
    * S&P processor (#54)
    
    * Added s&p processor
    
    * Added the option to run the pipeline multiple times (#51)
    
    * Added the option to run the pipeline multiple times
    
    * Deleted random stuff
    
    * Added the option to run the pipeline multiple times
    
    * Deleted 2 lines that were redundant
    
    * Translator processor (#55)
    
    * Added a translator processor
    
    * Added a translator processor
    
    * add translator to toml and made a split text workaround for the translator >1000
    
    * fix roberta bug <514 words
    
    ---------
    
    Co-authored-by: RaphaW <raphael-weber-bvs@gmx.de>
    
    * Fixed typo
    
    Co-authored-by: Max Buchholz <Max.Buchholz@gmx.de>
    
    * changed from & to and
    
    * Added s&p processor
    
    * changed from & to and
    
    * Fixed typo
    
    Co-authored-by: Max Buchholz <Max.Buchholz@gmx.de>
    
    ---------
    
    Co-authored-by: LeviUniAcc <102458061+LeviUniAcc@users.noreply.github.com>
    Co-authored-by: Max Buchholz <Max.Buchholz@gmx.de>
    
    * Added the poisson processor
    
    * Added the poisson processor
    
    * Added the poisson processor
    
    * Added the poisson processor
    
    ---------
    
    Co-authored-by: LeviUniAcc <102458061+LeviUniAcc@users.noreply.github.com>
    Co-authored-by: Max Buchholz <Max.Buchholz@gmx.de>

---
+++ src/fool_ai_detector/service/poisson_processor.py
@@ -0,0 +1,15 @@
+import numpy as np
+import skimage
+
+from fool_ai_detector.model.processor import Processor
+
+
+class PoissonProcessor(Processor):
+    def process(self, input_file_path, output_file_path):
+        """
+        Adds some random noise to an image to augment it.
+        """
+        image = skimage.io.imread(input_file_path)
+        processed_image = skimage.util.random_noise(image, mode='poisson', clip=True)
+        processed_image = (processed_image * 255).astype(np.uint8)
+        skimage.io.imsave(output_file_path, processed_image, check_contrast=False)
