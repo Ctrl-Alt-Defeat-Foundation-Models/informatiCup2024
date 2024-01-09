31f96526699679088218b88dfebafb2eaae7aa79
Max Buchholz <Max.Buchholz@gmx.de>
2024-01-09 10:02:44 +0100

    Refactor plotter.py: - make use of full names of libraries - reformat the file

--- src/fool_ai_detector/service/plotter.py
+++ src/fool_ai_detector/service/plotter.py
@@ -1,6 +1,6 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+import numpy
+import pandas
+import matplotlib.pyplot as plot
 
 ai_human_color = "orangered"
 human_ai_color = "crimson"
@@ -9,30 +9,33 @@ ai_ai_color = "darkorange"
 
 
 def read_csv(file_path_param):
-    return pd.read_csv(file_path_param)
+    return pandas.read_csv(file_path_param)
 
 
-def filter_data(filtered_data_param, text_or_image=None, time_interval=None, pipeline_processors=None, pipeline_generator=None):
+def filter_data(filtered_data_param, text_or_image=None, time_interval=None, pipeline_processors=None,
+                pipeline_generator=None):
     """
     whitelist filter of data
     
     :param filtered_data_param:
     :param text_or_image: 
     :param time_interval: 
-    :param pipeline_processors: 
-    :return: 
+    :param pipeline_processors:
+    :param pipeline_generator:
+    :return: filtered_data_param
     """
     if text_or_image is not None:
         filtered_data_param = filtered_data_param[filtered_data_param['text_or_image'] == text_or_image]
     if time_interval is not None:
-        start_time, end_time = pd.to_datetime(time_interval[0]), pd.to_datetime(time_interval[1])
+        start_time, end_time = pandas.to_datetime(time_interval[0]), pandas.to_datetime(time_interval[1])
         filtered_data_param = filtered_data_param[
-            pd.to_datetime(filtered_data_param['current_date_time']).between(start_time, end_time)]
+            pandas.to_datetime(filtered_data_param['current_date_time']).between(start_time, end_time)]
     if pipeline_processors is not None:
         filtered_data_param = filtered_data_param[filtered_data_param['pipeline_processors'].isin(pipeline_processors)]
     if pipeline_generator is not None:
         filtered_data_param = filtered_data_param[filtered_data_param['pipeline_generator'] == pipeline_generator]
     return filtered_data_param
+
 
 def plot_overview_pipeline_new(data, group_by_param):
     """
@@ -66,50 +69,43 @@ def plot_overview_pipeline_new(data, group_by_param):
         for index_inner, (group_name_inner, group_data_inner) in enumerate(grouped_data_inner):
             group_data_sum = group_data_inner[categories].sum()
 
-            combination_pipeline.append(str(group_name_outer[0])+"\n"+str(group_name_inner[0]))
+            combination_pipeline.append(str(group_name_outer[0]) + "\n" + str(group_name_inner[0]))
 
             ai_ai.append(group_data_sum["ai_ai"])
             human_human.append(group_data_sum["human_human"])
             ai_human.append(group_data_sum["ai_human"])
             human_ai.append(group_data_sum["human_ai"])
 
-        last_x_index = last_x_index + 1 # extra group distance
+        last_x_index = last_x_index + 1  # extra group distance
 
     # show diagram
     bar_width = .5
 
     bottom_of_bar = [0 for _ in ai_human]
-    p1 = plt.bar(x_location, ai_human, bottom=bottom_of_bar, width=bar_width, color="green")
+    p1 = plot.bar(x_location, ai_human, bottom=bottom_of_bar, width=bar_width, color="green")
 
     bottom_of_bar = ai_human
-    p2 = plt.bar(x_location, human_human, bottom=bottom_of_bar, width=bar_width, color="grey")
+    p2 = plot.bar(x_location, human_human, bottom=bottom_of_bar, width=bar_width, color="grey")
 
-    bottom_of_bar = np.add(human_human, bottom_of_bar).tolist()
-    p3 = plt.bar(x_location, ai_ai, bottom=bottom_of_bar, width=bar_width, color="black")
+    bottom_of_bar = numpy.add(human_human, bottom_of_bar).tolist()
+    p3 = plot.bar(x_location, ai_ai, bottom=bottom_of_bar, width=bar_width, color="black")
 
-    bottom_of_bar = np.add(ai_ai, bottom_of_bar).tolist()
-    p4 = plt.bar(x_location, human_ai, bottom=bottom_of_bar, width=bar_width, color="red")
+    bottom_of_bar = numpy.add(ai_ai, bottom_of_bar).tolist()
+    p4 = plot.bar(x_location, human_ai, bottom=bottom_of_bar, width=bar_width, color="red")
 
     # style
-    plt.ylabel('Amount')
-    plt.xlabel('Processors & Evaluators')
-    plt.title('Overview over multiple combination_pipeline \n (gaussian/poisson/s_and_p/speckle = 1/2/3/4, umm_maybe/resnet/nahrawy = 1/2/3)')
-    #plt.title('Overview over multiple combination_pipeline \n ("  "/typo = 1/2, roberta/radar = 1/2)')
-    plt.xticks(x_location, combination_pipeline)
-    plt.legend((p1[0], p2[0], p3[0], p4[0]), categories)
+    plot.ylabel('Amount')
+    plot.xlabel('Processors & Evaluators')
+    plot.title(
+        'Overview over multiple combination_pipeline \n (gaussian/poisson/s_and_p/speckle = 1/2/3/4, umm_maybe/resnet/nahrawy = 1/2/3)')
+    # plt.title('Overview over multiple combination_pipeline \n ("  "/typo = 1/2, roberta/radar = 1/2)')
+    plot.xticks(x_location, combination_pipeline)
+    plot.legend((p1[0], p2[0], p3[0], p4[0]), categories)
 
-    plt.show()
-
-
-
+    plot.show()
 
 
-
-
-
-"""
-change the values of the 1.filters and 2.filepath, then run this file.
-"""
+# change the values of the 1.filters and 2.filepath, then run this file.
 # example plots
 file_path = 'data.csv'
 data_frame = read_csv(file_path)
@@ -117,7 +113,7 @@ data_frame = read_csv(file_path)
 # filters, change these
 time_interval_filter = ('2024-01-03 00:00:00', '2024-03-05 00:00:00')
 pipeline_processor_filter = ['4']
-group_pipeline_processor_filter = None #['1', '2', '3', '4', ]
+group_pipeline_processor_filter = None  # ['1', '2', '3', '4', ]
 group_pipeline_generator_filter = 'dallE_generator_image'
 text_or_image_filter = 'image'
 
