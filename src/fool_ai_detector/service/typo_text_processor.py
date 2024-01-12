da90fbb355f676568355729ea531f7a30a686d86
RaphaW <raphael-weber-bvs@gmx.de>
2024-01-12 20:55:48 +0100

    Updated typo processor

--- src/fool_ai_detector/service/typo_text_processor.py
+++ src/fool_ai_detector/service/typo_text_processor.py
@@ -14,26 +14,25 @@ class TypoProcessor(Processor):
     Typo Processor
     """
 
-    def process(self, input_file_path, output_file_path, words_per_error=5):
+    def process(self, input_file_path, output_file_path, mistake_percentage=5):
         """
         Add some typos to the generated text and saves it.
 
         :param input_file_path: path to the file, that should be processed
         :param output_file_path: path where the processed file should be saved
-        :param words_per_error: number of consecutive words that contain one error
+        :param mistake_percentage: percentage of mistakes in the sentence
         """
         text = Path(input_file_path).read_text(encoding="utf-8")
         augmented_text = ''
 
         text_array = text.split()
         number_words = len(text_array)
-        five_percentage = number_words/100 * 5
-        five_percentage = math.ceil(five_percentage)
-        number_mistakes = random.sample(range(0, number_words), five_percentage)
+        percentage = math.ceil(number_words/100 * mistake_percentage)
+        number_mistakes = random.sample(range(0, number_words), percentage)
         loop_invariant = 0
         for word in text_array:
             if loop_invariant in number_mistakes:
-                type_of_error = random.randint(0, 8)
+                type_of_error = random.randint(0, 7)
                 error_string = typo.StrErrer(word, seed=31)
                 match type_of_error:
                     case 0:
@@ -46,8 +45,6 @@ class TypoProcessor(Processor):
                         augmented_text += error_string.extra_char(preservelast=False, preservefirst=True).result
                     case 4:
                         augmented_text += error_string.similar_char().result
-                    case 5:
-                        augmented_text += error_string.skipped_space().result
                     case 6:
                         augmented_text += error_string.random_space().result
                     case 7:
