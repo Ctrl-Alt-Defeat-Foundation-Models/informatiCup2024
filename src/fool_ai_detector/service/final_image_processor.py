85123ee35b4c6180a840c164d143b34a475be231
LukasMueller3 <lukas.mueller@nc-online.de>
2024-01-13 14:21:21 +0100

    First version of final image processor

--- src/fool_ai_detector/service/final_image_processor.py
+++ src/fool_ai_detector/service/final_image_processor.py
@@ -1,28 +1,25 @@
 """
 Final image processor based on the results of the tests
 """
-import numpy as np
-import skimage
 
 from fool_ai_detector.model.processor import Processor
+from fool_ai_detector.service.gaussian_processor import GaussianProcessor
+from fool_ai_detector.service.s_and_p_processor import SAndPProcessor
 
 
 class FinalImageProcessor(Processor):
     """
-    Image processor that uses...
-    - [Processor 1]
-    - [Processor 2]
-    - ...
+    Image processor that uses
+    - gaussian noise
+    - salt and pepper noise
     """
 
     def process(self, input_file_path, output_file_path):
         """
-        Augments an image by ...
+        Augments an image by adding gaussian and salt and pepper noise
 
         :param input_file_path: path to the file, that should be processed
         :param output_file_path: path where the processed file should be saved
         """
-        image = skimage.io.imread(input_file_path)
-        processed_image = skimage.util.random_noise(image, mode='gaussian', clip=True)
-        processed_image = (processed_image * 255).astype(np.uint8)
-        skimage.io.imsave(output_file_path, processed_image, check_contrast=False)
+        GaussianProcessor().process(input_file_path, output_file_path)
+        SAndPProcessor().process(output_file_path, output_file_path)
